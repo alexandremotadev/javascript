0cd530fa6d2dcd8d86e6e7efb4dfcd3b68df3e74
alexandremotadev <110003495+alexandremotadev@users.noreply.github.com>
2023-05-20 17:40:53 -0300

    exercicios 08, 09 e 10

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+// Atalho console NodeJS: F8
+
+var vel = 20
+console.log(`A velocidade do carro é ${vel}km/h`)
+if (vel > 60) { //Condição Simples
+  console.log("Volte ultrapassou a velocidade permitida. MULTADO!")
+}
+console.log("Use sempre o cinto de segurança!")
