a3cd002db7d5866724b2002b08302311d51905c6
alexandremotadev <110003495+alexandremotadev@users.noreply.github.com>
2023-05-21 19:44:45 -0300

    aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+
+/* ESTRUTURA DE REPETIÇÃO = ENQUANTO
+var c = 1
+while (c <= 5) {
+  console.log(`Passo ${c}`)
+  c++ // c = c + 1
+} */
+
+// ESTRUTURA DE REPETIÇÃO = FAÇA, ENQUANTO
+var c = 1
+
+do {
+  console.log(`Passo ${c}`)
+  c++ // c = c + 1
+} while (c <= 5)
---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <= 5) {
+  console.log(c)
+  c++
+}
+*/
+
+// c inicia em 1, enquanto c <= 10, soma c + 1 e repete
+// só sai da repetição quando c for maior que 10
+for (var c = 1; c <= 10; c++) {
+  console.log(c)
+} 
